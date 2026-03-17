07ee709ec1db14f53277107d3053551dcd136189
Khushi Bandekar <khushi.22310718@viit.ac.in>
2026-03-17 15:53:25 +0530

    removed unwanted images

--- Backend/controler/shopownercontrol.js
+++ Backend/controler/shopownercontrol.js
@@ -1,332 +1,234 @@
-const Users = require('../modules/shopownerModel')
-const Products = require('../modules/productModel')
-const fs = require('fs')
-
-const bcrypt = require('bcrypt')
-const jwt =require('jsonwebtoken')
-const cloudinary = require('cloudinary')
+const Users = require('../modules/shopownerModel');
+const Products = require('../modules/productModel');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cloudinary = require('cloudinary');
+const nodemailer = require('nodemailer');
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
-})
+});
 
-const shopownercontrol ={
-    register:async (req,res)=>{
-        console.log("register")
-        try{
-            const {name,lastname,phone,email,password,shopname,shopadd,zip} = req.body; 
-            const user = await Users.findOne({email})
-            if(user)
-                 return res.status(400).json({msg:"THIS EMAIL IS ALREADY EXIST"})
-            if(password.length <6) 
-                return res.status(400).json({msg:"THIS PASSWORD IS TOO WEAK"})
-            
-            const passwordhash = await bcrypt.hash(password,10)
-            const newuser  = new Users({
-                name,lastname,phone,email,password:passwordhash,shopname,shopadd,zip
-            })
-        //    console.log(newuser)
-           
-           // save mongodb
-           await newuser.save()
-           // then create jsonwebtoken to auth
-           const accesstoken = createAccessToken({id:newuser._id})
-          //  then create refresh token 
-          const refreshtoken = createRefreshToken({id:newuser._id})
-          res.cookie('refreshtoken',refreshtoken,
-          {
-            httpOnly:true,
-            path:'/user/refresh_token',
-            maxAge:7*24*60*60*1000
-          })
-           res.json({accesstoken})
-        }catch(err)
-        {
-            return res.status(500).json({msg:err.message})
-        }
-
-    },
-    login:async (req,res) =>{
-        console.log("login")
-        try{
-            const {email,password} = req.body;
-            const user = await Users.findOne({email})
-            if(!user)       return res.status(400).json({msg:"THIS User don't  EXISTS"});
-            const isMatch = await bcrypt.compare(password,user.password)
-            if(!isMatch) return res.status(400).json({msg:"Incorrect Password"});
-            // if login success, creat access token and refresh token
-            const accesstoken = createAccessToken({id:user._id})
-            const refreshtoken = createRefreshToken({id:user._id})
-          res.cookie('refreshtoken',refreshtoken,
-          {
-            httpOnly:true,
-            path:'/user/refresh_token',
-            maxAge:7*24*60*60*1000
-
-          })
-            res.json({accesstoken})
-        }
-        catch(err)
-        {
-            return res.status(500).json({msg:err.message})
-
-
-        }
-    },
-    
-    add:async (req,res) =>{
-        console.log("add")
-        try{
-            const {name,about,prize,url,N} = await req.body;
-            //console.log(name)
-           // console.log(req.user.id)
-
-            const shopowner  = await Users.findById({"_id":req.user.id}) 
-          //  console.log(shopowner)
-            const newproduct = new Products({name,about,prize,"email":shopowner.email,url,N})
-          //  console.log(newproduct)
-            await newproduct.save();
-            res.json({newproduct})
-       
-        }
-        catch(err)
-        {
-            return res.status(500).json({msg:err.message})
-
-
-        }
-    },
-    
-    photo:async (req,res) =>{  
-        console.log("photo adding")
+const shopownercontrol = {
+    register: async (req, res) => {
+        console.log("register");
         try {
-        if(!req.files || Object.keys(req.files).length === 0)
-            return res.status(400).json({msg: 'No files were uploaded.'})
-        
-        const file = req.files.file;
-        if(file.size > 1024*1024) {
-            removeTmp(file.tempFilePath)
-            return res.status(400).json({msg: "Size too large"})
-        }
-        // only takes jpg 
+            const { name, lastname, phone, email, password, shopname, shopadd, zip } = req.body;
+            const user = await Users.findOne({ email });
+            if (user) return res.status(400).json({ msg: "THIS EMAIL IS ALREADY EXIST" });
+            if (password.length < 6) return res.status(400).json({ msg: "THIS PASSWORD IS TOO WEAK" });
 
-  //      if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
-    //        removeTmp(file.tempFilePath)
-      //      return res.status(400).json({msg: "File format is incorrect."})
-        //}
+            const passwordhash = await bcrypt.hash(password, 10);
+            const newuser = new Users({
+                name, lastname, phone, email, password: passwordhash, shopname, shopadd, zip
+            });
 
-        cloudinary.v2.uploader.upload(file.tempFilePath, {folder: "test"}, async(err, result)=>{
-            if(err) throw err;
+            await newuser.save();
+            const accesstoken = createAccessToken({ id: newuser._id });
+            const refreshtoken = createRefreshToken({ id: newuser._id });
 
-            removeTmp(file.tempFilePath)
-
-            res.json({public_id: result.public_id, url: result.secure_url})
-        })
-
-
-    } catch (err) {
-        return res.status(500).json({msg: err.message})
-    }
-    },
-destroy:async (req, res) =>{
-    try {
-        console.log("phot remove")
-        const {public_id} = req.body;
-        if(!public_id) return res.status(400).json({msg: 'No images Selected'})
-
-        cloudinary.v2.uploader.destroy(public_id, async(err, result) =>{
-            if(err) throw err;
-
-            res.json({msg: "Deleted Image"})
-        })
-
-    } catch (err) {
-        return res.status(500).json({msg: err.message})
-    }
-    
-},
-    redata:async (req,res) =>{
-        console.log("readd")
-        try{
-            const {name,lastname,phone,age,gst,bank,ifsc,add1,add2,add3} = await req.body;
-            const shopowner  = await Users.findByIdAndUpdate({"_id":req.user.id},{"name":name,"lastname":lastname,"phone":phone,"age":age,"gst":gst,"ifsc":ifsc,"bank":bank,"add1":add1,"add2":add2,"add3":add3}) 
-
-       //     console.log(shopowner)
-            shopowner.save()
-            res.json({shopowner})
-       
-        }
-        catch(err)
-        {
-            return res.status(500).json({msg:err.message})
-
-
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            });
+            res.json({ accesstoken });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
         }
     },
-    
-    
-    all:async (req,res) =>{
-        console.log("add")
-        try{
-         
-       //     console.log(req.user.id)
 
-            const shopowner  = await Users.findById({"_id":req.user.id}) 
-     //       console.log(shopowner)
-            const newproduct = await Products.find({"email":shopowner.email})
-            res.json({"list":newproduct})
-        }
-        catch(err)
-        {
-            return res.status(500).json({msg:err.message})
+    login: async (req, res) => {
+        console.log("login");
+        try {
+            const { email, password } = req.body;
+            const user = await Users.findOne({ email });
+            if (!user) return res.status(400).json({ msg: "THIS User don't EXISTS" });
 
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) return res.status(400).json({ msg: "Incorrect Password" });
 
+            const accesstoken = createAccessToken({ id: user._id });
+            const refreshtoken = createRefreshToken({ id: user._id });
+
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            });
+            res.json({ accesstoken });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
         }
     },
-    refreshtoken:async (req,res)=>{
-        console.log("refreshtoken")
-        try{
+
+    add: async (req, res) => {
+        console.log("add product data");
+        try {
+            const { name, about, prize, url, N } = req.body;
+            const shopowner = await Users.findById(req.user.id);
+            const newproduct = new Products({ name, about, prize, "email": shopowner.email, url, N });
+            await newproduct.save();
+            res.json({ newproduct });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
+        }
+    },
+
+    photo: async (req, res) => {
+        console.log("photo adding to cloudinary");
+        try {
+            if (!req.files || Object.keys(req.files).length === 0)
+                return res.status(400).json({ msg: 'No files were uploaded.' });
+
+            const file = req.files.file;
+
+            if (file.size > 1024 * 1024) {
+                removeTmp(file.tempFilePath);
+                return res.status(400).json({ msg: "Size too large (Max 1MB)" });
+            }
+
+            // Using Promise to properly catch Cloudinary errors
+            const result = await new Promise((resolve, reject) => {
+                cloudinary.v2.uploader.upload(file.tempFilePath, { folder: "test" }, (err, res) => {
+                    if (err) reject(err);
+                    else resolve(res);
+                });
+            });
+
+            removeTmp(file.tempFilePath);
+            res.json({ public_id: result.public_id, url: result.secure_url });
+
+        } catch (err) {
+            console.error("Cloudinary Error:", err);
+            return res.status(500).json({ msg: "Cloudinary upload failed: " + err.message });
+        }
+    },
+
+    destroy: async (req, res) => {
+        try {
+            console.log("photo remove");
+            const { public_id } = req.body;
+            if (!public_id) return res.status(400).json({ msg: 'No images Selected' });
+
+            await cloudinary.v2.uploader.destroy(public_id);
+            res.json({ msg: "Deleted Image" });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
+        }
+    },
+
+    redata: async (req, res) => {
+        console.log("update shopowner data");
+        try {
+            const { name, lastname, phone, age, gst, bank, ifsc, add1, add2, add3 } = req.body;
+            const shopowner = await Users.findByIdAndUpdate(req.user.id, {
+                name, lastname, phone, age, gst, ifsc, bank, add1, add2, add3
+            }, { new: true });
+
+            res.json({ shopowner });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
+        }
+    },
+
+    all: async (req, res) => {
+        try {
+            const shopowner = await Users.findById(req.user.id);
+            const products = await Products.find({ "email": shopowner.email });
+            res.json({ "list": products });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
+        }
+    },
+
+    refreshtoken: async (req, res) => {
+        try {
             const rf_token = req.cookies.refreshtoken;
-            if(!rf_token) return res.status(400).json({message:"Plase Login or Register"});
-            jwt.verify(rf_token,process.env.REFRESH_TOKEN_SECRET,(err,user)=>
-            {
-                if(err) return res.status(400).json({message:"PLEASE LOGIN OR rEGISTER"})
-                const accesstoken =  createAccessToken({id:user.id})
-                res.json({accesstoken})
-            })
-           
-        }catch(err){
-            return res.status(500).json({msg:err.message})
+            if (!rf_token) return res.status(400).json({ message: "Please Login or Register" });
 
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+                if (err) return res.status(400).json({ message: "PLEASE LOGIN OR REGISTER" });
+                const accesstoken = createAccessToken({ id: user.id });
+                res.json({ accesstoken });
+            });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
         }
     },
-    info:async (req,res)=>{
-        console.log("infor")
-        try{
-         
-        //    console.log(req.user.id)
 
-            const user  = await Users.findById({"_id":req.user.id}) 
-            
-            res.json({"user":user})
+    info: async (req, res) => {
+        try {
+            const user = await Users.findById(req.user.id);
+            res.json({ "user": user });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
         }
-        catch(err)
-        {
-            return res.status(500).json({msg:err.message})
-
-        }
-    
     },
-    done:async (req,res)=>{
-        console.log("done")
-        try{
-            const {val} = req.body
-            const user  = await Users.findById({"_id":req.user.id})
-            var k = user.order
-            console.log(k.length)
 
-            var l=[] ;
-            for(var i =0;i<k.length;i++)
-            {
-
-                if(!((k[i].name  === val.name )&& (k[i].order ===  val.order ) &&  (k[i].date ===  val.date ) ))
-                {
-                    l.push(k[i])
-                }
-                else{
-                    console.log(k[i].name)
-                }
-            }
-            console.log(l.length)
-            await Users.findByIdAndUpdate({"_id":user._id},{"order":l})
-            res.json({"user":user})
-
+    done: async (req, res) => {
+        try {
+            const { val } = req.body;
+            const user = await Users.findById(req.user.id);
+            let orders = user.order.filter(item => 
+                !((item.name === val.name) && (item.order === val.order) && (item.date === val.date))
+            );
+            await Users.findByIdAndUpdate(user._id, { "order": orders });
+            res.json({ "user": user });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
         }
-        catch(err)
-        {
-            return res.status(500).json({msg:err.message})
-
-
-        }
-    
     },
-    notdone:async (req,res)=>{
-        try{
 
-            const {val} = req.body
-            console.log(val)
-            const user  = await Users.findById({"_id":req.user.id})
-            var k = user.order
-            var l=[] ;
-            for(var i =0;i<k.length;i++)
-            {
-                if(!((k[i].name  === val.name )&&(k[i].order ===  val.order )&&(k[i].date ===  val.date ) ))
-                {
-                    l.push(k[i])
-                }
-                else
-                {
-                    console.log(k[i].name)
-                }
-            }
+    notdone: async (req, res) => {
+        try {
+            const { val } = req.body;
+            const user = await Users.findById(req.user.id);
+            let orders = user.order.filter(item => 
+                !((item.name === val.name) && (item.order === val.order) && (item.date === val.date))
+            );
+            await Users.findByIdAndUpdate(user._id, { "order": orders });
 
-            await Users.findByIdAndUpdate({"_id":user._id},{"order":l})
-            var start ='<h1 style="text-align: center ; background-color:red;" >Your Pooja is Cancled</h1>'
-            var k =    '<div style="display: flex; align-items: center; background-color:red;border-style: solid; color: white;"><div style="flex: 1; text-align: left;"><img src="'+val.poojaurl+'" alt="product image " width="250px" height="250px"  /></div><div style="color: white;margin: auto;width: 50%;border: 3px solid green;padding: 10px;"> <h1> Name : '+val.order+'</h1><h1>Price : '+val.poojaprize+'.00 ₹</h1> </div></div>'
-            var transporter = await nodemailer.createTransport({
+            let transporter = nodemailer.createTransport({
                 service: 'gmail',
                 auth: {
-                  user: process.env.Email,
-                  pass: process.env.Password
-                },port:465,
-                host:'smtp.gmail.com'
-              });
-               var mailOptions =  await {
-                from: process.env.Email, 
-                to:val.email,
-                subject: "A pooja is canneclleded ",
-                html: start+k
-                };
-          
+                    user: process.env.Email,
+                    pass: process.env.Password
+                },
+                port: 465,
+                host: 'smtp.gmail.com'
+            });
 
-      await transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } 
-        else {
-          console.log('Email sent: ' + info.response);
+            let mailOptions = {
+                from: process.env.Email,
+                to: val.email,
+                subject: "Your Pooja Order has been Cancelled",
+                html: `<h1 style="text-align: center; background-color:red;">Order Cancelled</h1>
+                       <div style="display: flex; align-items: center; border: 1px solid red; padding: 10px;">
+                           <img src="${val.poojaurl}" width="150" height="150" />
+                           <div style="margin-left: 20px;">
+                               <h2>Name: ${val.order}</h2>
+                               <h3>Price: ₹${val.poojaprize}.00</h3>
+                           </div>
+                       </div>`
+            };
+
+            await transporter.sendMail(mailOptions);
+            res.json({ "user": user });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
         }
-      });
-    res.json({"user":user})
+    }
+};
 
+const createRefreshToken = (user) => jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+const createAccessToken = (user) => jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
 
-        }
-        catch(err)
-        {
-            return res.status(500).json({msg:err.message})
-            console.log(err)
+const removeTmp = (path) => {
+    fs.unlink(path, err => { if (err) console.error(err); });
+};
 
-        }
-    
-    
-}
-}
-
-const createRefreshToken =(user)=>
-{
-    return jwt.sign(user,process.env.REFRESH_TOKEN_SECRET,{expiresIn:'7d'})
-}
-const createAccessToken =(user)=>
-{
-    return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'})
-}
-
-
-const removeTmp = (path) =>{
-    fs.unlink(path, err=>{
-        if(err) throw err;
-    })
-}
-module.exports = shopownercontrol
+module.exports = shopownercontrol;
